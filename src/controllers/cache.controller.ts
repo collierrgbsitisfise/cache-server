e003927f741f278c76d04d9082a752c09373647f
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2018-10-28 12:04:39 +0200

    implement redis service

--- src/controllers/cache.controller.ts
+++ src/controllers/cache.controller.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
+import RedisClient from "./../services/redis.service";
+
+const RedisC = new RedisClient("redis://127.0.0.1:6379");
+RedisC.connect();
 
 export const setDataByKey = async (req: Request, res: Response) => {
   try {
     const { key, data } = req.body;
-    res.send(new Date());
+    RedisC.setExpValue(key, data, 60 * 60);
+    res.send("ok");
   } catch (err) {
     res.status(500).send(err);
   }
@@ -12,7 +17,8 @@ export const setDataByKey = async (req: Request, res: Response) => {
 export const getDataByKey = async (req: Request, res: Response) => {
   try {
     const { key } = req.params;
-    res.send(key);
+    let data = await RedisC.getValue(key);
+    res.send(data);
   } catch (err) {
     res.status(500).send(err);
   }
---
+++ src/services/redis.service.ts
@@ -0,0 +1,53 @@
+import * as redis from "redis";
+import * as bluebird from "bluebird";
+bluebird.promisifyAll(redis);
+
+class RedisClient {
+  public host: string;
+  private redisClient: any = null;
+
+  constructor(host: string) {
+    this.host = host;
+  }
+
+  public connect(): void {
+    this.redisClient = redis.createClient(this.host);
+  }
+
+  public setValue(key: string, value: any): void {
+    if (typeof value !== "string") {
+      value = JSON.stringify(value);
+    }
+
+    this.redisClient.set(key, value);
+  }
+
+  public setExpValue(
+    key: string,
+    value: any,
+    seconds: number = 60 * 60 * 24
+  ): void {
+    if (typeof value !== "string") {
+      value = JSON.stringify(value);
+    }
+
+    this.redisClient.set(key, value, "EX", seconds);
+  }
+
+  public async getValue(key: string): Promise<any> {
+    try {
+      let result = await this.redisClient.getAsync(key);
+      return {
+        data: result,
+        error: null
+      };
+    } catch (err) {
+      return {
+        data: null,
+        error: err
+      };
+    }
+  }
+}
+
+export default RedisClient;
